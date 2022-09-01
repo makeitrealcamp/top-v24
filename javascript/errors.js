1873f3c7cb2e220a2b6652ca760497f3dc47ef27
Sergio <sergio@bellpi.com>
2022-09-01 13:39:47 -0500

    Exepciones, flexbox, resposive web

---
+++ javascript/errors.js
@@ -0,0 +1,52 @@
+// const _name = "Jhon Doe";
+// console.log(_name);
+
+// _name();
+
+/* function divide(a, b) {
+  if (b === 0) {
+    throw new ReferenceError("Ups esto va ser indeterminado");
+  }
+
+  return a / b;
+}
+
+console.log(divide(1, 0)); */
+
+/* const student = "Esteban";
+student = "Jairo"; */
+
+/* let a =5, b, c
+
+for(let i = 0; i < 10, i++){
+
+} */
+
+/* function divide(a, b) {
+  if (b === 0) {
+    throw new ReferenceError("Ups esto va ser indeterminado");
+  }
+
+  return a / b;
+}
+
+divide(1, 0); */
+
+/* try {
+  divide(1, 5);
+  console.log("divide 1");
+  divide(1, 3);
+  console.log("divide 2");
+  divide(1, 52);
+  console.log("divide 3");
+  divide(1, 0);
+  console.log("Todo bien");
+} catch (error) {
+  console.log("algo salio mal");
+  console.log(error.name);
+  console.log(error.message);
+} */
+
+//Como pedir ayuda ?
+// Lo que pasa es que hice una función para dividir y cuando la ejecuto me lanza un error de referencia
+// Intente solucionarlo, agregando una error pero sigue sucediendo ademas de eso estuve consultando en internet y tambien intente las siguiente soluciones https://stackoverflow.com/questions/2075337/uncaught-referenceerror-is-not-defined
---
+++ javascript/formato/index.js
@@ -0,0 +1,6 @@
+function hello2() {
+  const username = 2;
+  return username;
+}
+
+hello2();
