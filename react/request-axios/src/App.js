d9274df6f70af6570e7be3a59aa9e95c6eb5a989
Sergio <sergio@bellpi.com>
2022-09-07 10:14:41 -0500

    clases request

---
+++ react/request-axios/src/App.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+//GET --> https://api-airbnbclon.com/hosts --> Pedir TODAS las Hospedajes
+//GET --> https://api-airbnbclon.com/hosts/:id --> Pedir UNA SOLA hospedaje
+//POST --> https://api-airbnbclon.com/hosts --> Crear un solo recurso
+//PUT --> https://api-airbnbclon.com/hosts/:id --> Actualizar un hospedaje
+//DELETE --> https://api-airbnbclon.com/hosts/:id --> Eliminar un hospedaje 
+
+function App() {
+
+  const [posts, setPosts] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    //GET
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        //entra si el status es entre 100 - 399 - positivo
+        setPosts(res.data)
+      }).catch((err) => {
+        //entra si el status es entre 400 - 599 - negativo
+        alert(`ups hay un error ${err.message}, comuniquese a servicio al cliente 31054897466`)
+      }).finally(() => {
+        //entra siempre
+        setLoading(false)
+      })
+  }, [])
+
+  /*   fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(json => console.log("FETCH", json)) */
+
+
+  return (
+    <div className="App">
+      <h1>Clase de HTTP y peticiones a un servidor</h1>
+      {loading ? <p>Loading...</p> : posts.map((item) => {
+        return (
+          <div key={item.id}>
+            <h2>{item.title}</h2>
+            <p>{item.body}</p>
+          </div>
+        )
+      })}
+    </div>
+  );
+}
+
+export default App;
