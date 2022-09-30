ef4d3a021f737b617464ce6522e23caf2e184c52
Sergio <sergio@bellpi.com>
2022-09-30 08:28:18 -0500

    configuración proyecto para testing

--- testing/backend/src/server.js
+++ testing/backend/src/server.js
@@ -16,12 +16,6 @@ app.use(cors())
 app.use(morgan("tiny"))
 
 app.use("/auth/local", userRouter)
-app.get("/", auth, (req, res) => {
-  // const { userId } = req.params // Esta ya no es la forma
-  const userId = req.user
-  res.status(200).json({ name: "Jairo", age: 27, city: "Barranquilla", email: "jairo@test.com" })
-})
-
 
 app.listen(port, () => {
   console.log(`App running at http://localhost:${port}`)
