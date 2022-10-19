4b15dcc7c0e69abfed877c999878fca877b70a7b
Sergio <sergio@bellpi.com>
2022-10-19 16:44:01 -0500

    actualizaciòn auth

--- authentication/backend/src/api/user.model.js
+++ authentication/backend/src/api/user.model.js
@@ -21,7 +21,7 @@ const userSchema = new Schema(
     password: {
       type: String,
       required: true
-    }
+    },
   },
   { timestamps: true }
 )
--- authentication/backend/src/server.js
+++ authentication/backend/src/server.js
@@ -5,6 +5,7 @@ const morgan = require("morgan")
 const { connect } = require("./database")
 const { auth } = require("./utils/auth")
 const userRouter = require("./api/user.route")
+const User = require("./api/user.model")
 
 
 const port = process.env.PORT;
@@ -16,10 +17,19 @@ app.use(cors())
 app.use(morgan("tiny"))
 
 app.use("/auth/local", userRouter)
-app.get("/", auth, (req, res) => {
-  // const { userId } = req.params // Esta ya no es la forma
-  const userId = req.user
-  res.status(200).json({ name: "Jairo", age: 27, city: "Barranquilla", email: "jairo@test.com" })
+app.get("/", auth, async (req, res) => {
+  try {
+    const userId = req.user
+    const user = await User.findById(userId)
+
+    res.status(201).json({
+      data: {
+        ...user
+      }
+    })
+  } catch (err) {
+
+  }
 })
 
 
